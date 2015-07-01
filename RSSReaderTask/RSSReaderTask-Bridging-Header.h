20d3222ff87abd45b256258e4ac3cd2212cda947
ShikataniTomonori <shikatani.0902@gmail.com>
2015-07-02 01:00:34 +0900

    環境構築：Bridging Headerを作成

---
+++ RSSReaderTask/RSSReaderTask-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  RSSReaderTask-Bridging-Header.h
+//  RSSReaderTask
+//
+//  Created by ShikataniTomonori on 2015/07/02.
+//  Copyright (c) 2015年 ShikataniTomonori. All rights reserved.
+//
+
+#ifndef RSSReaderTask_RSSReaderTask_Bridging_Header_h
+#define RSSReaderTask_RSSReaderTask_Bridging_Header_h
+
+
+#endif
